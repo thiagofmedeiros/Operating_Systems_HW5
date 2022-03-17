e376fc4f5da11e16e0fb8d1bebf2fddc2046dcfa
tmedeiros <thiago.f.medeiros@gmail.com>
2022-03-16 20:03:14 -0400

    working

--- main.c
+++ main.c
@@ -1,6 +1,104 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//#define IP_ADDR "10.247.53.97"   /* osnode16.cse.usf.edu IP address, where server must run */
+#define IP_ADDR "127.0.0.1"
+#define PORT_NUM 10501
+#define BUFFLEN 100
+#define n 3
+
+struct threadDat {
+    int threadNumber;
+    int connectionSocket;
+};
+
+char buf[BUFFLEN];
+pthread_mutex_t mutex;
+
+void *connection_thread(void *args) {
+    char inBuffer[BUFFLEN];
+    struct threadDat *arguments = args;
+
+    printf("Connection %d open\n", arguments->threadNumber);
+
+    int len = read(arguments->connectionSocket, inBuffer, BUFFLEN);
+
+    printf("Connection %d Received: %s\n", arguments->threadNumber, inBuffer);
+
+    if (pthread_mutex_trylock(&mutex) == 0) {
+        for (int i = 0; i < len; i++) {
+            buf[i] = inBuffer[i];
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    sleep(2);
+
+    send(arguments->connectionSocket, buf, len, 0);
+
+    return (NULL);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int connectionSocket;
+    int addressLength;
+    struct sockaddr_in serverAddress;
+    int fd;
+
+    pthread_t connections[n];     /* process id for threads */
+    pthread_mutex_init(&mutex,NULL);
+
+    /* serverAddress naming for the internet domain socket */
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT_NUM);
+    serverAddress.sin_addr.s_addr = inet_addr(IP_ADDR);
+
+    addressLength = sizeof (serverAddress);
+
+    /* socket() call generates one socket for ipc */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("Opening Stream Socket");
+        exit(1);
+    }
+
+    if (bind(fd, (struct sockaddr *)&serverAddress, addressLength) < 0) {
+        printf("Bind error");
+        exit(1);
+    }
+
+    if (listen(fd, 3) < 0) {
+        printf("Listen error");
+        exit(1);
+    }
+
+    int i = 0;
+    while (i < n) {
+        connectionSocket = accept(fd, (struct sockaddr *) &serverAddress,
+                                  (socklen_t *) &addressLength);
+        if (connectionSocket < 0) {
+            perror("Accept error");
+        } else {
+            struct threadDat *args = malloc(sizeof *args);
+
+            args->connectionSocket = connectionSocket;
+            args->threadNumber = i;
+
+            pthread_create(&connections[i], NULL, connection_thread, args);
+
+            i++;
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        pthread_join(connections[i], NULL);
+    }
+
+    close(fd);
+
     return 0;
 }
---
+++ writern/writercom.c
@@ -0,0 +1,84 @@
+/* This is the client part for sending one file through network stream socket 
+ and waiting for its return.  
+This writer can run anywhere. 
+gcc writern.c -lsocket -lxnet
+clang and most gcc (like on osnodes) does not want any libraries included (there by default)
+explicit library calls are required depending on the OS.
+
+4/13/17: Writes from info from Command line. */
+
+
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define IP_ADDR "10.247.53.97"   /* osnode16's IP address, where server 
+                                    must run */
+/* For test #define PORT_NUM 1150 */
+/* #define PORT_NUM 1050 */
+//#define PORT_NUM 1044
+//#define PORT_NUM 1050
+//#define PORT_NUM 1159
+//#define PORT_NUM 1051
+//#define PORT_NUM 1050
+#define PORT_NUM 10501
+#define BUFFLEN 100
+
+int  main(int argc, char *argv[])
+{
+	int len;
+	int sock;
+	struct sockaddr_in  server,remote;
+	int remotelength;
+	int fd;
+	char buf[BUFFLEN];
+	if ( argc != 2 ) /* argc should be 2 for correct execution */
+	  {
+	    /* We print argv[0] assuming it is the program name */
+	    printf( "usage: %s filename", argv[0] );
+            exit(0);
+	  }
+	/* socket() call generates one socket for ipc */
+	if ( (sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Opening Stream Socket");
+		exit(1);
+	}
+
+	/* address naming for the internet domain socket */
+	server.sin_family = AF_INET;
+      	server.sin_port = htons(PORT_NUM);
+      	server.sin_addr.s_addr = inet_addr(IP_ADDR);
+
+	/* use the socket and server address to initiate connection */
+	if( connect(sock, (struct sockaddr*)&server, sizeof(server))<0)	{
+		perror("connecting stream socket");
+		exit(1);
+	}
+
+
+
+        len = strlen(argv[1]);
+        printf("Connected and sending out a message of len %d\n", len);
+
+		if (send(sock, argv[1], len, 0) == -1)
+			{
+			perror("Write to Socket Channel");
+			exit(1);
+			}
+        len = recv(sock, buf, BUFFLEN, 0);
+        printf("Got back: \n");
+        fflush(stdout);
+        write(0, buf, len);
+        printf("\nEnd msg*** \n");
+	/* close the communication socket and listenning socket */
+	close(sock);
+
+}
+
